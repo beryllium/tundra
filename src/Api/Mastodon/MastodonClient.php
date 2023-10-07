6c3ba4ee238651dece6a81d3b0924ae3c5e9642c
Kevin Boyd <kevin.boyd@gmail.com>
2023-10-07 10:45:18 -0700

    Do a better job of building URLs, and also return as an associative array

--- src/Api/Mastodon/MastodonClient.php
+++ src/Api/Mastodon/MastodonClient.php
@@ -4,16 +4,21 @@ namespace Whateverthing\Tundra\Api\Mastodon;
 
 class MastodonClient
 {
-    public function __construct(public string $server)
-    {
-    }
+    public function __construct(public string $server) {}
 
     public function request(...$args): mixed
     {
         $url = 'https://' . ($args['server'] ?? $this->server) . '/api/v1/' . ($args['apiMethod']);
 
+        unset($args['server'], $args['apiMethod']);
+
+        $args = array_filter($args);
+        if (count($args) > 0) {
+            $url = $url . '?' . http_build_query($args);
+        }
+
         $result = file_get_contents($url);
 
-        return json_decode($result);
+        return json_decode($result, true);
     }
 }

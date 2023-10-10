f6dfca95c7f0016e677734d89b84498fcb2ee386
Kevin Boyd <kevin.boyd@gmail.com>
2023-10-10 00:16:09 -0700

    Try to explain various exceptions if `.env` doesn't exist
    - Might need some additional tweaking here to skip dying if the
      auth command is being called.

--- app/env.php
+++ app/env.php
@@ -7,6 +7,13 @@ const TUNDRA_ENV_FILE = TUNDRA_ROOT . '/.env';
 
 $dotenv = new Dotenv();
 $dotenv->usePutenv();
-$dotenv->load(__DIR__ . '/../.env');
+
+try {
+    $dotenv->load(realpath(TUNDRA_ENV_FILE));
+} catch (\Symfony\Component\Dotenv\Exception\FormatException $e) {
+    die('Formatting error found in your .env file! ' . realpath(TUNDRA_ENV_FILE));
+} catch (\Symfony\Component\Dotenv\Exception\PathException $e) {
+    die('Your .env file could not be found or is not readable! ' . realpath(TUNDRA_ENV_FILE));
+}
 
 return $dotenv;

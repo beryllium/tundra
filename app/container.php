26887d0b344caa597eed2a48521215d923c074e8
Kevin Boyd <kevin.boyd@gmail.com>
2023-10-10 22:22:53 -0700

    Return a fully-configured MastodonClient if the settings are present

--- app/container.php
+++ app/container.php
@@ -14,6 +14,14 @@ $container
     ->invokeMethod('setContainer', [$container]);
 
 $container->add(MastodonClient::class, function () {
-    return new MastodonClient(server: getenv('MASTODON_SERVER'));
+    return new MastodonClient(
+        server: getenv(MastodonClient::CONFIG_SERVER),
+        username: getenv(MastodonClient::CONFIG_USERNAME),
+        appId: getenv(MastodonClient::CONFIG_APP_ID),
+        clientId: getenv(MastodonClient::CONFIG_CLIENT_ID),
+        clientSecret: getenv(MastodonClient::CONFIG_CLIENT_SECRET),
+        clientAccessToken: getenv(MastodonClient::CONFIG_CLIENT_ACCESS_TOKEN),
+        userAccessToken: getenv(MastodonClient::CONFIG_USER_TOKEN)
+    );
 });
 return $container;
